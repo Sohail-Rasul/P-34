6ba4b6cd3089ebb43988960fa75c7a8cee72b52e
Sohail-Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2020-10-03 14:35:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var dog,happydDog,database,foodStock,foodS;
+
+
+
+
+function preload(){
+  dogimg = loadImage("images/dogImg.png");
+  dogimg2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  var canvas = createCanvas(500,500 );
+  database = firebase.database();
+  foodStock = database.ref('food');
+  foodStock.on("value",readStock);
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogimg);
+  dog.scale = 0.2;
+ 
+}
+
+
+function draw() {  
+background(46, 139, 87);
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS)
+  dog.addImage(dogimg2);
+}
+
+
+
+
+
+  drawSprites();
+  fill("red");
+  textSize(20);
+  stroke(5);
+  text("Press Up Arrow Kew To Feed Drago milk",100,100);
+  
+  
+  
+
+}
+function readStock(data){
+  foodS=data.val();
+}
+function writeStock(x){
+database.ref('/').update({
+  food:x
+})
+}
+
+
